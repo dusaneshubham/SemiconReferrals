042edcbea7dd1959b60e1ce9425d90c54eb62c5f
Denis-Shingala <denisshingala@gmail.com>
2022-10-30 10:23:36 +0530

    v-1.0.1

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -13,6 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
+dotenv.config();
 
 // Defining Routes
 app.use("/admin", adminRoute);
@@ -20,9 +22,9 @@ app.use("/candidate", candidateRoute);
 app.use("/company", companyRoute);
 
 mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(() => console.log("Connected to Database"))
     .catch((err) => {
         console.log(err);
