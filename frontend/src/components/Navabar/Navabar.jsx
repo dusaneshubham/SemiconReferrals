cfeabdbdb18a6c97dd012dc229536233fe20f953
Denis-Shingala <denisshingala@gmail.com>
2022-11-05 00:34:02 +0530

    v1.2.0

---
+++ frontend/src/components/Navabar/Navabar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navabar = () => {
+    return (
+        <>
+            <div>Navabar</div>
+        </>
+    )
+}
+
+export default Navabar;
