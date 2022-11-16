1bcf5455daac314492c1efcef692e52226f67cd6
Denis-Shingala <denisshingala@gmail.com>
2022-11-16 12:04:52 +0530

    Navbar Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import Navbar from './components/Navbar/NavbarComponent';
+import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import Footer from './components/Footer/Footer';
 
