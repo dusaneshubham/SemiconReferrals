0b721fd1261d6096ce9c682cf47365bd6c8b8390
Shubham <dusaneshubham>
2022-12-25 14:14:27 +0530

    testing v1.2

--- frontend/src/components/CandidateListing/CandidateList/CandidateList.jsx
+++ frontend/src/components/CandidateListing/CandidateList/CandidateList.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./candidate-list.css";
-import { image2 } from "../../../images/images";
 import { Link } from "react-router-dom";
 
 const CandidateList = (props) => {
   return (
     <>
-      <div className="card text-center m-2 shadow-lg" style={{ width: "18rem" }}>
-        <div className="card-title p-2 fs-5 bg-light rounded-top" style={{ color: "var(--main-blue)" }}>
+      <div
+        className="card text-center m-2 shadow-lg"
+        style={{ width: "18rem" }}
+      >
+        <div
+          className="card-title p-2 fs-5 bg-light rounded-top"
+          style={{ color: "var(--main-blue)" }}
+        >
           {props.data.name}
         </div>
         <div className="img-part py-3">
-          {props.data.candidateinfo[0] && props.data.candidateinfo[0].profileImage ? (
+          {props.data.candidateinfo[0] &&
+          props.data.candidateinfo[0].profileImage ? (
             <img
               src={`http://localhost:5000/profileImage/${props.data.candidateinfo[0].profileImage}`}
               alt="Candidate Profile"
@@ -21,7 +27,7 @@ const CandidateList = (props) => {
             />
           ) : (
             <img
-              src={image2}
+              src="http://localhost:5000/profileImage/defaultImage.png"
               alt="Candidate Profile"
               style={{ borderRadius: "50%" }}
               width="100"
@@ -31,9 +37,12 @@ const CandidateList = (props) => {
         </div>
         {/* <img src="..." className="card-img-top" alt="..." /> */}
         <div className="card-body w-100 p-2 bg-light rounded-bottom">
-          <Link to={`/candidate/viewprofile/${props.data._id}`} className="text-dark">
+          <Link
+            to={`/candidate/viewprofile/${props.data._id}`}
+            className="text-dark"
+          >
             View Profile
-          </Link >
+          </Link>
         </div>
       </div>
     </>
--- frontend/src/components/EmployerListing/EmployerList/EmployerList.jsx
+++ frontend/src/components/EmployerListing/EmployerList/EmployerList.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import "./employer-list.css";
-import { image2 } from "../../../images/images";
 import { Link } from "react-router-dom";
 
 const EmployerList = (props) => {
   return (
     <>
-      <div className="card text-center m-2 shadow-lg" style={{ width: "18rem" }}>
-        <div className="card-title p-2 fs-5 bg-light rounded-top" style={{ color: "var(--main-blue)" }}>
+      <div
+        className="card text-center m-2 shadow-lg"
+        style={{ width: "18rem" }}
+      >
+        <div
+          className="card-title p-2 fs-5 bg-light rounded-top"
+          style={{ color: "var(--main-blue)" }}
+        >
           {props.data.name}
         </div>
         <div className="img-part py-3">
-          {props.data.recruiterinfo[0] && props.data.recruiterinfo[0].profileImage ? (
+          {props.data.recruiterinfo[0] &&
+          props.data.recruiterinfo[0].profileImage ? (
             <img
               src={`http://localhost:5000/profileImage/${props.data.recruiterinfo[0].profileImage}`}
               alt="Recruiter Profile"
@@ -21,7 +27,7 @@ const EmployerList = (props) => {
             />
           ) : (
             <img
-              src={image2}
+              src="http://localhost:5000/profileImage/defaultImage.png"
               alt="Recruiter Profile"
               style={{ borderRadius: "50%" }}
               width="100"
@@ -31,9 +37,12 @@ const EmployerList = (props) => {
         </div>
         {/* <img src="..." className="card-img-top" alt="..." /> */}
         <div className="card-body w-100 p-2 bg-light rounded-bottom">
-          <Link to={`/employer/viewprofile/${props.data._id}`} className="text-dark">
+          <Link
+            to={`/employer/viewprofile/${props.data._id}`}
+            className="text-dark"
+          >
             View Profile
-          </Link >
+          </Link>
         </div>
       </div>
     </>
